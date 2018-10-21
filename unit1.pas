97874fc8b16f4c74b077b9781ee7372c11e13421
Simon Fonteneau <blog@lesfourmisduweb.org>
2018-10-21 22:54:24 +0200

    run on enter

--- unit1.pas
+++ unit1.pas
@@ -19,6 +19,8 @@ type
     serverandport: TLabeledEdit;
     procedure ConnexionClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure serverandportChange(Sender: TObject);
+    procedure serverandportKeyPress(Sender: TObject; var Key: char);
   private
 
   public
@@ -42,17 +44,36 @@ begin
 
 end;
 
-procedure TFormmstsc.ConnexionClick(Sender: TObject);
+procedure TFormmstsc.serverandportChange(Sender: TObject);
 begin
   ListServerAndPort := TStringList.Create;
   ListServerAndPort.Delimiter := ':';
   ListServerAndPort.DelimitedText := serverandport.Text;
-  server:= ListServerAndPort[0] ;
+  if int(ListServerAndPort.Count) > 0
+  then
+    server:= ListServerAndPort[0]
+  else
+    server:= '';
+end;
+
+procedure runxfreerdp;
+begin
   if int(ListServerAndPort.Count) > 1 then
       port:= ListServerAndPort[1]
   else
       port:= '3389';
   ExecuteProcess('/usr/bin/xfreerdp',['/v:' + Server ,'/port:' + port ,'/sec:rdp','&']);
+end;
+
+procedure TFormmstsc.serverandportKeyPress(Sender: TObject; var Key: char);
+begin
+  if Key = #13 then
+    runxfreerdp;
+end;
+
+procedure TFormmstsc.ConnexionClick(Sender: TObject);
+begin
+  runxfreerdp;
 end;
 
 end.
